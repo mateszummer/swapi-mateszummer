c9761da60091981673d99746fd5cc280d1e95d63
mateszummer <szummerm@gmail.com>
2017-10-12 22:28:19 +0200

    psql connetion issue

--- data_manager.py
+++ data_manager.py
@@ -11,7 +11,7 @@ def open_database():
     try:
         parse.uses_netloc.append("postgres")
         url = parse.urlparse(os.environ["DATABASE_URL"])
-        conn = psycopg2.connect(
+        connection = psycopg2.connect(
         database=url.path[1:],
         user=url.username,
         password=url.password,
